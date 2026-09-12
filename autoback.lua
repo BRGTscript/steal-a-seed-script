b46a63e0cd3836d23c5fc5fff5675b51d7452a97
BRGTscript <papajan.silleza@gmail.com>
2026-09-13 07:40:07 +0800

    Update autoback.lua

--- autoback.lua
+++ autoback.lua
@@ -1,4 +1,5 @@
-local AutoTeleport = true
+
+getgenv().AutoTeleport = getgenv().AutoTeleport or false
 local speedfores = 500
 
 local targetCFrame = CFrame.new(3.18183708, 3.59631872, -84.4609451, 0.999309659, 1.08471054e-08, -0.0371507965, -9.55431823e-09, 1, 3.49758942e-08, 0.0371507965, -3.45967983e-08, 0.999309659)
@@ -7,11 +8,10 @@ local player = game:GetService("Players").LocalPlayer
 
 local function executeTeleport()
     task.spawn(function()
-                task.wait(0.2)
+        task.wait(0.2)
 
         local char = player.Character
         if not char then return end
-        
         
         local hrp = char:FindFirstChild("HumanoidRootPart")
         if not hrp then return end
@@ -20,7 +20,7 @@ local function executeTeleport()
         local steps = math.ceil(dist / (speedfores / 10))
 
         for i = 1, steps do
-                        if not hrp or not hrp.Parent then break end
+            if not getgenv().AutoTeleport or not hrp or not hrp.Parent then break end
 
             local alpha = i / steps
             hrp.CFrame = hrp.CFrame:Lerp(targetCFrame, alpha)
@@ -37,7 +37,8 @@ rawMetatable.__namecall = newcclosure(function(self, ...)
     local args = {...}
     local method = getnamecallmethod()
 
-    if AutoTeleport and method == "FireServer" and args[1] == "StealEnemyEgg" then
+
+    if getgenv().AutoTeleport and method == "FireServer" and args[1] == "StealEnemyEgg" then
         executeTeleport()
     end
 
