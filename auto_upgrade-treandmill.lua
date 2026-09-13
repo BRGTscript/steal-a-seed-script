4b0edb70cd18221276326b6b5898687ecdd302ac
BRGTscript <papajan.silleza@gmail.com>
2026-09-14 05:31:13 +0800

    Create auto_upgrade-treandmill.lua

---
+++ auto_upgrade-treandmill.lua
@@ -0,0 +1,19 @@
+getfenv().gayson = getfenv().gayson or false
+
+
+
+task.spawn(function()
+while getfenv().gayson do
+task.wait(0.3)
+
+
+local Event = game:GetService("ReplicatedStorage").RemoteEvent.ServerRemoteEvent
+Event:FireServer(
+    "Business",
+    "\xE8\xAE\xAD\xE7\xBB\x83\xE6\x9C\xBA\xE5\x99\xA8\xE8\xA7\xA3\xE9\x94\x81\xE5\x8D\x87\xE7\xBA\xA7"
+)
+
+
+
+  end
+   end)
